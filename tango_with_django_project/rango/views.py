c6cb3b10ce077838481a5a1f1b1ba98ece0e5078
Dazai-kun <huycutetunglxl@gmail.com>
2018-05-29 20:40:06 +0700

    added

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'category': category_list}
+
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     context_dict2 = {'my_name': 'Huy'}
