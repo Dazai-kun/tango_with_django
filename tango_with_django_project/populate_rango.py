ccfd65394869ee8a4fbd9d41dd774dbb26795792
Dazai-kun <huycutetunglxl@gmail.com>
2018-05-29 16:17:50 +0700

    added

---
+++ tango_with_django_project/populate_rango.py
@@ -0,0 +1,55 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+from rango.models import Category, Page
+def populate():
+    #python_cat = add_cat('Python', views=128, likes=64)
+    python_pages = [
+        {'title': 'Official Python Tutorial',
+          'url': 'http://docs.python.org/2/tutorial/' },
+        {'title': 'How to think like a Computer Scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/'},
+        {'title': 'Learn python in 10mins',
+         'url': 'http://www.korokithakis.net/tutorials/python/'}
+]
+    #django_cat = add_cat('Django', views=64, likes=32)
+    django_pages = [
+    {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/1.9/intro/tutorial01/' },
+    {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/'},
+    {'title': 'How to Tango with Djano', 'url': 'http://www.tangowithdjango.com/'}
+    ]
+    #other_cat = add_cat('Other Frameworks', views=32, likes=16)
+    other_pages = [
+    {'title': 'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
+    {'title': 'Flask', 'url':'http://flask.pocoo.org'}
+    ]
+    cats = {'Python': {'pages': python_pages}, 'Django': {'pages': django_pages}, 'Other Frameworks': {'pages': other_pages} }
+#there r now 3 Categories, each category has the pages as above.
+    for cat, cat_data in cats.items(): #cats.items = [('Python', ('pages', 'python_pages')]
+        c = add_cat(cat) # this will basically add the categories Python, Djano and Other Frameworks
+        for p in cat_data['pages']: # cat_data[pages] = python_pages
+            add_page(c, p['title'], p['url']) # it will add the pages into the category
+
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print('- {0} - {1}'.format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title) [0] #create page according to the 3 categories by title
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name, likes=16, views=32):
+    c = Category.objects.get_or_create(name=name, likes=16, views=32) [0] # create the categories by name
+    c.views=views
+    c.likes=likes 
+    c.save() # then save it
+    return c
+
+if __name__ == '__main__':
+    print('Starting Rango population script.....')
+    populate()
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+TEMPLATE_DIR= os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -101,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL =  '/media/'
